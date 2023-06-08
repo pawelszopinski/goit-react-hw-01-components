30aaa825bc19ed0850300a21cdd5a2df8478d22e
Paweł Szopiński <szopinskipawel@gmail.com>
2023-06-09 00:09:34 +0200

    Task 1&2 in progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
+import user from './user.json';
+import data from './data.json';
+export const Profile = ({
+  username = user.username,
+  tag = user.tag,
+  location = user.location,
+  avatar = user.avatar,
+  stats = user.stats,
+}) => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div class="profile">
+      <div class="description">
+        <img src={avatar} alt="User avatar" class="avatar" />
+        <p class="name">{username}</p>
+        <p class="tag">{tag}</p>
+        <p class="location">{location}</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{stats.likes}</span>
+        </li>
+      </ul>
     </div>
+  );
+};
+export const Statistics = ({ title, stats = {} }) => {
+  return (
+    <section class="statistics">
+      <h2 class="title">Upload stats</h2>
+
+      <ul class="stat-list">
+        <li class="item">
+          <span class="label">.docx</span>
+          <span class="percentage">4%</span>
+        </li>
+        <li class="item">
+          <span class="label">.mp3</span>
+          <span class="percentage">14%</span>
+        </li>
+        <li class="item">
+          <span class="label">.pdf</span>
+          <span class="percentage">41%</span>
+        </li>
+        <li class="item">
+          <span class="label">.mp4</span>
+          <span class="percentage">12%</span>
+        </li>
+      </ul>
+    </section>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { Profile } from 'components/App';
+import { Statistics } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Profile />
+    <Statistics />
   </React.StrictMode>
 );
